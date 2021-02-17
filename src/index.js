4a8ee006bcc3330d4821c41c82f53f732a3faf7f
Myrian Katto <myriankatto@gmail.com>
2021-02-17 21:35:33 +0100

    cors issue

--- src/index.js
+++ src/index.js
@@ -18,11 +18,14 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://imagible.herokuapp.com');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
   next();
 });
 
