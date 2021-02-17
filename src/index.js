a1af3cba8caf94e842d562c5c825436f7453449b
Myrian Katto <myriankatto@gmail.com>
2021-02-17 21:32:20 +0100

    cors issue

--- src/index.js
+++ src/index.js
@@ -18,14 +18,11 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type,Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 
