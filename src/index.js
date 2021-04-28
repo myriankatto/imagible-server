38236a988cf3c4c1fea26019149b1ba6784af035
Myrian Katto <myriankatto@gmail.com>
2021-04-28 19:09:05 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
 
 /**
  * Database setup
@@ -17,6 +16,27 @@ mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://imagible.herokuapp.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
