8f8e3dee3759fc624035abf2338a17d465f67f14
Myrian Katto <myriankatto@gmail.com>
2021-04-28 17:48:35 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -17,6 +17,19 @@ mongoose.connect(process.env.MONGO_URL, {
   useCreateIndex: true,
 });
 
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTION');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
